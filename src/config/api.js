a6d32cfd3ab6a74f82cb517b1c916d4ae0ab7fab
KOTIPALLI PRAVEEN KUMAR <2200032132@kluniversity.in>
2025-06-20 15:54:32 +0530

    added deployed backend link

--- src/config/api.js
+++ src/config/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const API_BASE_URL = "http://localhost:5454";
+export const API_BASE_URL = "https://project-management-system-with-spring-301x.onrender.com/";
 
 const api = axios.create({baseURL: API_BASE_URL});
 
